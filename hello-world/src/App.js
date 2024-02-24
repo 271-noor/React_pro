8d932096b8f137d31f2134896cf6c1cd11185d50
271-noor <157873714+271-noor@users.noreply.github.com>
2024-02-24 13:01:33 +0530

    Added NavbarComponent

--- hello-world/src/App.js
+++ hello-world/src/App.js
@@ -1,46 +1,18 @@
 // import React, {Component} from 'react';
-import logo from './logo.svg';
+// import logo from './logo.svg';
+import Navbar from './component/Navbar'
 import './App.css';
-import  Greet  from './component/Greet'
-import  Welcome  from './component/welcome'
-import Hello from './component/Hello'
-import Message from './component/message'
-import  Counter  from './component/counter'
-import FunctionClick from './component/FunctionClick'
-import ClassClick from './component/ClassClick'
-import EventBind from './component/EventBind'
-import ParentComponent from './component/ParentComponent'
-import UserGreeting from './component/UserGreeting'
-import NameList from './component/NameList'
- 
-
 
 function App (){
 
   return (
+<>
     <div className="App">
-          <NameList />
-          {/* <UserGreeting /> */}
-          {/* <ParentComponent /> */}
-        {/* <EventBind /> */}
-        {/* <ClassClick /> */}
-      {/* <FunctionClick /> */}
-        {/* <Counter /> */}
-        {/* <Message /> */}
-
-        {/* <Greet name="Bruce" heroName="Badman" >
-         <p>This is Children Props</p>
-        </Greet> */}
-      {/* <Greet name="Clark" heroName="Superman">
-        <button>Action</button>
-        </Greet> */}
-       {/* <Greet name="Diana" heroName="Wonder Woman" /> */}
-        {/* <Welcome name="Bruce" heroName="Badman" /> */}
-       {/* <Welcome name="Clark" heroName="Superman" /> */}
-       {/* <Hello /> */}
-
+       <NameList />
+      {/* <Navbar title="Textutils" AboutText="About textUtils" /> */}
       </div>
-  )
+      </>
+  );
   }
 
 
--- hello-world/src/new.js
+++ hello-world/src/new.js
@@ -1,5 +1,16 @@
 // import logo from './logo.svg';
 // import './App.css';
+import  Greet  from './component/Greet'
+import  Welcome  from './component/welcome'
+import Hello from './component/Hello'
+import Message from './component/message'
+import  Counter  from './component/counter'
+import FunctionClick from './component/FunctionClick'
+import classClick from './component/classClick'
+import EventBind from './component/EventBind'
+import ParentComponent from './component/ParentComponent'
+import UserGreeting from './component/UserGreeting'
+import NameList from './component/NameList'
 
 // function App() {
 //   return (
@@ -19,6 +30,25 @@
 //         </a>
 //       </header>
 //     </div>
+         
+          {/* <UserGreeting /> */}
+          {/* <ParentComponent /> */}
+        {/* <EventBind /> */}
+        {/* <classClick /> */}
+      {/* <FunctionClick /> */}
+        {/* <Counter /> */}
+        {/* <Message /> */}
+
+        {/* <Greet name="Bruce" heroName="Badman" >
+         <p>This is Children Props</p>
+        </Greet> */}
+      {/* <Greet name="Clark" heroName="Superman">
+        <button>Action</button>
+        </Greet> */}
+       {/* <Greet name="Diana" heroName="Wonder Woman" /> */}
+        {/* <Welcome name="Bruce" heroName="Badman" /> */}
+       {/* <Welcome name="Clark" heroName="Superman" /> */}
+       {/* <Hello /> */}
 //   );
 // }
 
