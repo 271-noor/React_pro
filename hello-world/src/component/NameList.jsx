b95e9942bcc8e2c46a65d89004f675cdbc7b9a27
271-noor <157873714+271-noor@users.noreply.github.com>
2024-02-24 14:55:28 +0530

    using index in li

--- hello-world/src/component/NameList.jsx
+++ hello-world/src/component/NameList.jsx
@@ -1,9 +1,11 @@
 // List Rendering (Map  List)
 
 import React from 'react'
+import Person from './Person'
 
 function NameList() {
     // create array of Component
+    const names = ['Bruce', 'Clark', 'Diana', 'Preeti']
     const persons = [
       {
         id: 1,
@@ -22,18 +24,19 @@ function NameList() {
         name: 'Diana',
         age: 28,
         skill: 'Vue'
-      },
+      }
     ]
-
     // declare a new constant Const nameList and assign the result of the Map Operation
-    const personList = persons.map(person => <Person person={person} />
-    )
+    
+    // const personList = persons.map(person => <Person key = {person.id} person={person} />)
+    const nameList = names.map((name, index) => <h2 key={index}> {index} {name} </h2>)
   return (
     <div>
         {/*(After map Operation..)
          then in the return statement. I'll simply include the name list in the jsx */}
             {
-                personList
+                // personList
+                nameList
             }
     </div>
   )
